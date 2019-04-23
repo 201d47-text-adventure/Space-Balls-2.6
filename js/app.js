25dd0b3ca3fab7056078169397fe90f97783f1a5
Jon Rice <revyolution1120@gmail.com>
2019-04-23 08:49:44 -0700

    Background colors for playbox and buttons

--- js/app.js
+++ js/app.js
@@ -22,7 +22,7 @@ choice2.addEventListener('click', endGame);
 //--------------------Intro--------------------------//
 var introText = document.getElementById('textinsert');
 function introductionText() {
-  introText.textContent = 'Pilot the world is in a dire situation. Over the past few years melting of the polar ice caps has accelerated. Florida and parts of Italy are completely user the high sea levels. The world has banned together and found that is possible to establish ourselves on Mars. You have been elected to carry out the initial journey. Your resources will be limited for this journey. How you manage these resources will utlilmatly determine your success as well as the survival of the human race.';
+  introText.textContent = 'Pilot the world is in a dire situation. Over the past few years melting of the polar ice caps has accelerated. Florida and parts of Italy are completely under water. The world has banned together and found that is possible to establish ourselves on Mars. You have been elected to carry out the initial journey. Your resources will be limited for this journey. How you manage these resources will utlilmatly determine your success. The human race is counting on you in these trying times. Consider your choices carfeully to ensure the completion of your mission.';
   startButton.textContent = 'Start Adventure';
   insertStartButton.append(startButton);
 }
@@ -45,16 +45,6 @@ function choiceOne() {
   choiceBoxes.appendChild(choice2);
 }
 
-var choiceOneMade = function(event) {
-  event.preventDefault();
-  var choiceOneCorrect = document.getElementById('textinsert');
-  if (inputAnswers === 'go around' || inputAnswers ==='go around astroid belt'){
-    choiceOneCorrect.textContent = 'You continue around the belt saving your ship from being damaged';
-  } else {
-    var choiceOneIncorrect = document.getElementById('textinsert');
-    choiceOneIncorrect.textContent = 'Your ship has been damaged by space particle';
-  }
-};
 //--------------------Choice 2--------------------------//
 
 
